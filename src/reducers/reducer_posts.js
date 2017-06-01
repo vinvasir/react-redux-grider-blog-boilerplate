247f4cdc127fde4ec07987969c1eb9414adf93c7
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-06-01 16:10:09 -0400

    scaffold the blog functionality

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,13 @@
+import _ from 'lodash';
+import { FETCH_POSTS, CREATE_POST } from '../actions';
+
+export default function(state = {}, action) {
+	switch (action.type) {
+		case FETCH_POSTS:
+			return _.mapKeys(action.payload.data, 'id');
+		// case CREATE_POST:
+		// 	return state;
+		default:
+			return state;
+	}
+}
